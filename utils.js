dd87442a27f947f2b8fcb7f613220e4988f6e1a7
unknown <Fbio Santos@FbioSantos-PC.(none)>
2013-05-02 22:40:12 +0100

    functional

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
 'use strict'
 
+window.revealedTiles = []
+window.backOfBadge = 'https://i2.wp.com/codebits.eu/logos/defaultavatar.jpg'
+window.timeToLookBeforeHiding = 1000
+
 function onReadyStateChange () {
     if (document.readyState === 'complete') {
         onReady()
@@ -21,8 +25,7 @@ function fetchBadges() {
 }
 
 function onBadgesReceived(data) {
-    var backOfBadge = 'https://i2.wp.com/codebits.eu/logos/defaultavatar.jpg',
-        tableElement = document.getElementById('game-table'),
+    var tableElement = document.getElementById('game-table'),
         randomNineURLs,
         shuffledPairs,
         asHTML,
@@ -90,10 +93,53 @@ function prepareGame() {
 }
 
 function onClickOnTable(e) {
-    console.log(e)
-    // TODO find out which badge and call onClickOnBadge
+    // call the event function with the <img> element as `this`
+    onClickOnBadge.call(e.originalTarget)
+    return false
 }
 
-function onClickOnBadge(e) {
+function isRevealed(image) {
+    var found = false
+    for (var i = 0; i < window.revealedTiles.length; i++) {
+        if (window.revealedTiles[i] === image) {
+            found=true;
+            break;
+        }
+    }
+    return found
+}
+
+function onClickOnBadge() {
+    // Do not reveal already revealed badges. For that we try to find this img in the revealedTiles thing.
+    if (isRevealed(this)) {
+        return
+    }
     
+    // Do not reveal after revealing 2 images. There's a short interval.
+    if (window.revealedTiles.length === 2) {
+        return
+    }
+    
+    // Reveal
+    this.src = this.getAttribute('data-real-source')
+    
+    window.revealedTiles.push(this)
+    
+    if (window.revealedTiles.length === 2) {
+        // Check if the player found it.
+        if (revealedTiles[0].src === revealedTiles[1].src) {
+            // Unreveal nothing
+            window.revealedTiles = []
+        } else {
+            // Unreveal later
+            setTimeout(unReveal, window.timeToLookBeforeHiding)
+        }
+    }
+}
+
+function unReveal() {
+    window.revealedTiles.map(function (image) {
+        image.src = backOfBadge
+    })
+    window.revealedTiles = []
 }
